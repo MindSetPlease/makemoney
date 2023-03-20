bb93f7f46e6763bdd2e95a4f9074cc573fc6514a
Seungwon Bang <73030613+seungwonB@users.noreply.github.com>
2023-03-20 21:25:35 +0900

    연솝 합 / SILVER 2

---
+++ BAEKJOON/DP/20230320_1912_BSW.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, maxN;
+int dp[100001] = { 0 };
+int arr[100001] = { 0 };
+
+int main() {
+	cin >> n;
+		
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	dp[0] = arr[0];
+	
+	for (int i = 1; i < n; i++) {
+		dp[i] = max(arr[i] + dp[i-1], arr[i]);
+	}
+
+	maxN = dp[0];
+	
+	for (int i = 0; i < n; i++) {
+		if (dp[i] > maxN) maxN = dp[i];
+	}
+	
+	cout << maxN;
+}
